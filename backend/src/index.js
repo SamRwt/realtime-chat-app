14822bdffe8940c5dc9b68622b86f5f29d8065e6
Sameer Rawat <b22172@students.iitmandi.ac.in>
2025-03-20 10:47:08 +0530

    Add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 
 import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
@@ -13,6 +14,10 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
