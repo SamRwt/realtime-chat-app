3313f9f2808c569896aac2f61e7160da677e7783
Sameer Rawat <b22172@students.iitmandi.ac.in>
2025-03-27 10:16:52 +0530

    Add production environment code

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,10 @@ import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
+import path from "path";
+
 const port = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -21,6 +24,14 @@ app.use(cors({
 
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
